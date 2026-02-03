ad64f884320e97fcf83412714925f00807644917
rihsievisroil8-web <rihsievisroil8@gmail.com>
2026-02-03 09:35:22 +0500

    Foods and Work section finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Foods from "./sections/foods";
 import Header from "./sections/Header";
+import Work from "./sections/Work";
 
 export default function App() {
 	return (
 		<div>
 			<Header />
+			<Foods />
+			<Work/>
 		</div>
 	);
 }
---
+++ src/sections/Work.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Work.css";
+
+export default function Work() {
+    return (
+      <div className="aaa">
+        <h1>How does it work</h1>
+        <div className="div2">
+          <div className="card">
+            <img src="img2.png" alt="" />
+            <h5>Select location</h5>
+            <p>
+              Choose the location where your <br /> food will be delivered.
+            </p>
+          </div>
+          <div className="card">
+            <img src="img3.png" alt="" />
+            <h5>Select location</h5>
+            <p>
+              Choose the location where your <br /> food will be delivered.
+            </p>
+          </div>
+          <div className="card">
+            <img src="img4.png" alt="" />
+            <h5>Select location</h5>
+            <p>
+              Choose the location where your <br /> food will be delivered.
+            </p>
+          </div>
+          <div className="card">
+            <img src="img5.png" alt="" />
+            <h5>Select location</h5>
+            <p>
+              Choose the location where your <br />food will be delivered.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+}
